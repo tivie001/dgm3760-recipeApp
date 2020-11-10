7360329939e29508e731fce83c2919e559cfc953
Tyler Ivie <tylerivie@MacBook-Pro.local>
2020-11-09 17:07:52 -0700

    Added node module packages.

---
+++ models/listModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const RecipeSchema = new Schema({
+    title: String,
+    subTitle: String,
+    rating: String,
+    prepDetails: String,
+    prepHours: String,
+    prepMins: String,
+    totalHours: String,
+    totalMins: String,
+    difficulty: String,
+    ingredients: Array,
+    directions: Array,
+    // img: {
+    //     data: Buffer,
+    //     contentType: String
+    // }
+})
+
+module.exports = mongoose.model('Recipe', RecipeSchema);
+
