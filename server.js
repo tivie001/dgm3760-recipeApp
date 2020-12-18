d000285d6cb65f1a9378e39eafbc95424fb31c48
Tyler Ivie <tylerivie@MacBook-Pro.local>
2020-12-17 18:52:47 -0700

    Removed dotconfig

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Recipe = require('./models/recipeModel');
 const List = require('./models/listModel');
-require('dotenv/config');
+// require('dotenv/config');
 const app = express();
 const PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
